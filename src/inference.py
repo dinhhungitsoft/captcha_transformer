e7b75ac08c369ba8b146279e199f0177efb471d6
hung nguyen <dinhhungitsoft@gmail.com>
2022-03-16 20:34:51 +0700

    update

--- src/inference.py
+++ src/inference.py
@@ -16,17 +16,18 @@ def predict(img, model, lbl_encoder, device = "cpu", max_len = 5, label=""):
 
     for i in range(5):
         with torch.no_grad():         
-            for k in range(6 - len(L)):
-                L.append(0)   
-            trg_tensor = torch.LongTensor(L).unsqueeze(0).to(device)
-            trg_mask = model.make_src_mask(trg_tensor)
+            # for k in range(6 - len(L)):
+            #     L.append(0)   
+            trg_tensor = torch.LongTensor(predicts).unsqueeze(0).to(device)
+            trg_mask = model.make_trg_mask(trg_tensor)
+            # trg_mask=None
             output, _ = model.decoder( trg_tensor, encoded,trg_mask = trg_mask, src_mask=None)
             predicted = output.argmax(2)
-            idx = L.index(0) - 1
+            # idx = L.index(0) - 1
+            idx=-1
             predicted = predicted[:,idx].item()
 
-            predicts.append(predicted)
-            L = predicts.copy()
+            predicts.append(predicted)            
             
             abc = lbl_encoder.classes_[predicted]
             result.append(abc)
@@ -38,9 +39,10 @@ def predict(img, model, lbl_encoder, device = "cpu", max_len = 5, label=""):
 
 train_loader, test_loader, lbl_encoder, test_orig_targets = utils.prepare_data()
 model = build_model("/home/hung/learn/pytorch/captcha_trainsformer/weights/model_326.pt")
-
-
+print(model)
+d = 0
 for bindex, data in enumerate(test_loader):
+    
     for j in range(8):
         img = data["images"][j].unsqueeze(0)
         lbl = data["raw_targets"][j]
@@ -48,3 +50,6 @@ for bindex, data in enumerate(test_loader):
         lbl = lbl.replace(">", "")
         res = predict(img, model, lbl_encoder, "cpu", 5, lbl)
         print(f"{res}, {lbl}")
+        if res != lbl:
+            d+=1
+print(f"False predict {d}")    
